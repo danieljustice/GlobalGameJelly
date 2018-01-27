52197625fcb4d14639be2cb10641cdc8931f91d8
Daniel Justice <danieljustice09@gmail.com>
2018-01-27 11:21:59 -0500

    basic spawner and infrastructure set up

---
+++ Assets/Scripts/SpawnFunction.cs
@@ -0,0 +1,3 @@
+﻿using UnityEngine;
+
+public delegate void SpawnFunction(Spawner[] spawners);
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//put this on an empty game object and give feed 'pointPrefab' the prefab that you would like to spawn.
+//when spawn is called it will instantiate that prefab at the origin point of the empty prefab
+public class Spawner : MonoBehaviour {
+
+    public Transform pointPrefab;
+
+
+    public void Spawn()
+    {
+        Transform point = Instantiate(pointPrefab);
+
+        //keep position of the spawner transform
+        Vector3 position = Vector3.zero;
+        //keep scale of 1 of the spawner transform
+        Vector3 scale = Vector3.one;
+
+        point.SetParent(transform, false);
+
+    }
+}
