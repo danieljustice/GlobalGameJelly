95c4b84c5114acfa950a10970dd161e3f5b52987
otoolew <otoolew@gmail.com>
2018-01-27 20:19:08 -0500

    Basic Hand Charge Function

---
+++ Assets/Code/Controller/ChargeBoltBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChargeBoltBehavior : MonoBehaviour {
+    public Animator animator;
+    public string tagToCharge;
+    public Slider chargeSlider;
+    public float chargeAmount;
+    public GameObject chargedBoltGo;
+    
+	void Start () {       
+        animator.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        chargeAmount = chargeSlider.value;      	
+	}
+    public void ShowCharged()
+    {
+        chargedBoltGo.SetActive(true);
+    }
+    public void HideCharged()
+    {
+        chargedBoltGo.SetActive(false);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag.Equals(tagToCharge))
+        {
+            Debug.Log("Charging Hands!");
+            chargeSlider.value++;
+        }
+    }
+
+}
